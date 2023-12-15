d0c596b1f0c9ebc8748e49bb09fa6332b3d7046f
Ajay R <114697213+ajayr6696@users.noreply.github.com>
2023-12-15 10:59:36 -0500

    Update sortBy.js - Customer Issue 3

--- debug-test/server/utils/sortBy.js
+++ debug-test/server/utils/sortBy.js
@@ -1,44 +1,65 @@
 export default function sortBy(array, key, descending = false) {
-  console.log(array, key);
   const length = array.length;
-  if (length === 1) {
-    return array;
-  } else if (length === 2) {
-    const aValue = array[0][key];
-    const bValue = array[1][key];
-    if (bValue > aValue) {
+  // sorting by created time
+  if (key === 'created') {
+    if (length === 1) {
+      return array;
+    } else if (length === 2) {
+      const aValue = array[0][key];
+      const bValue = array[1][key];
+      if (bValue > aValue) {
+        return array;
+      }
+      return [array[0], array[1]];
+    }
+
+    const mid = Math.floor(length / 2);
+    const firstHalf = array.slice(0, mid);
+    const secondHalf = array.slice(mid, length);
+
+    const arrayOne = sortBy(firstHalf, key);
+    const arrayTwo = sortBy(secondHalf, key);
+
+    const merged = [];
+    while (arrayOne.length || arrayTwo.length) {
+      if (!arrayOne.length) {
+        merged.push(arrayTwo.shift());
+        continue;
+      }
+
+      if (!arrayTwo.length) {
+        merged.push(arrayOne.shift());
+        continue;
+      }
+
+      const valueOne = arrayOne[0][key];
+      const valueTwo = arrayTwo[0][key];
+      if (valueOne <= valueTwo) {
+        merged.push(arrayOne.shift());
+      } else if (valueTwo < valueOne) {
+        merged.push(arrayTwo.shift());
+      }
+    }
+
+    return descending ? merged.reverse() : merged;
+  } else if (key === 'votes') { // Sorting by votes
+    if (length <= 1) {
       return array;
     }
-    return [array[0], array[1]];
+
+    const sortVotes = (a, b) => {
+      const aValue = Number(a[key]);
+      const bValue = Number(b[key]);
+
+      if (descending) {
+        return bValue - aValue;
+      } else {
+        return aValue - bValue;
+      }
+    };
+
+    return array.slice().sort(sortVotes);
   }
-
-  const mid = Math.floor(length / 2);
-  const firstHalf = array.slice(0, mid);
-  const secondHalf = array.slice(mid, length);
-
-  const arrayOne = sortBy(firstHalf, key);
-  const arrayTwo = sortBy(secondHalf, key);
-
-  const merged = [];
-  while (arrayOne.length || arrayTwo.length) {
-    if (!arrayOne.length) {
-      merged.push(arrayTwo.shift());
-      continue;
-    }
-
-    if (!arrayTwo.length) {
-      merged.push(arrayOne.shift());
-      continue;
-    }
-
-    const valueOne = arrayOne[0][key];
-    const valueTwo = arrayTwo[0][key];
-    if (valueOne <= valueTwo) {
-      merged.push(arrayOne.shift());
-    } else if (valueTwo < valueOne) {
-      merged.push(arrayTwo.shift());
-    }
-  }
-
-  return descending ? merged.reverse() : merged;
+  // Default case for other types of sorting or keys
+  return [...array];
 }
